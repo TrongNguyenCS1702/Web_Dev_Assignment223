482d810623729ccd30bc4f47cfc74a5a345fa593
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-07-24 02:11:24 +0700

    update header

---
+++ View/Component/footer/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <link rel="stylesheet" href="style.css">
+  <script src="index.js"></script>
+  <title>Ivy</title>
+</head>
+<body>
+    <footer>
+        
+    </footer>
+    <script src="index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ View/Component/header/header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <link rel="stylesheet" href="header.css">
+  <script src="index.js"></script>
+  <title>Ivy</title>
+</head>
+<body>
+  <div class="container">
+
+    <header>  
+      <div class="logo">
+        <img src="/View/Component/images/logo.png" alt="logo">
+      </div>
+      <div class="menu">
+        <li><a href="#">NỮ</a>
+          <ul class="sub-menu">
+            <li><a href="#">Áo</a>
+              <ul>
+                <li><a href="">Áo sơ mi</li>
+                <li><a href="">Áo thun</a></li>
+                <li><a href="">Áo Vest</a></li>
+                <li><a href="">Áo khóac</a></li>
+                <li><a href="">Áo Len</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Quần</a>
+              <ul>
+                <li><a href="">Áo sơ mi</a></li>
+                <li><a href="">Áo thun</a></li>
+                <li><a href="">Áo Vest</a></li>
+                <li><a href="">Áo khóac</a></li>
+                <li><a href="">Áo Len</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Áo</a>
+              <ul>
+                <li><a href="">Áo sơ mi</a></li>
+                <li><a href="">Áo thun</a></li>
+                <li><a href="">Áo Vest</a></li>
+                <li><a href="">Áo khóac</a></li>
+                <li><a href="">Áo Len</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Áo</a>
+              <ul>
+                <li><a href="">Áo sơ mi</a></li>
+                <li><a href="">Áo thun</a></li>
+                <li><a href="">Áo Vest</a></li>
+                <li><a href="">Áo khóac</a></li>
+                <li><a href="">Áo Len</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Áo</a>
+              <ul>
+                <li><a href="">Áo sơ mi</a></li>
+                <li><a href="">Áo thun</a></li>
+                <li><a href="">Áo Vest</a></li>
+                <li><a href="">Áo khóac</a></li>
+                <li><a href="">Áo Len</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">NAM</a></li>
+        <li><a href="#">TRẺ EM</a></li>
+        <li><a href="#">SALE CUỐI TUẦN</a></li>
+        <li> <a href="#">BỘ SƯU TẬP</a></li>
+        <li><a href="#">LIFESTYLE</a></li>
+        <li><a href="#">VỀ CHÚNG TÔI</a></li>
+      </div>
+      <div class="orthers">
+        <div class="search">
+          <form action="#" method="get" enctype="#" name="form_search" class="form_search">
+            <input class="input" type="text" name="search" placeholder="Tìm kiếm " autocomplete="off" minlength="1">
+            <button class="submit"><i class="fas fa-search"></i></button>
+          </form>
+        </div>
+        <div class="item wallet">
+          <a class="icon" href="#"><i class="fas fa-headphones-alt"></i></a>
+        </div>
+        <div class="item wallet">
+          <a class="icon" href="#"><i class="fas fa-user"></i></a>
+        </div>
+        <div class="item wallet">
+          <a class="icon" href="#"><i class="fas fa-shopping-bag"></i></a>
+        </div>
+      </div>
+    </header>
+  </div>
+    <script src="header.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
