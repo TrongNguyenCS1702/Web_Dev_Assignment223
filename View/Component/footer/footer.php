b0f081cad860ea7dd6351d956e845adab07559b0
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-07-27 18:46:00 +0700

    update

--- View/Component/footer/footer.php
+++ View/Component/footer/footer.php
@@ -6,15 +6,52 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <link rel="stylesheet" href="style.css">
-  <script src="index.js"></script>
+  <link rel="stylesheet" href="footer.css">
+  <script src="footer.js"></script>
   <title>Ivy</title>
 </head>
 <body>
-    <footer>
-        
-    </footer>
-    <script src="index.js"></script>
+
+  <div class="container">
+  <footer>
+    <div class="main-footer d-flex">
+      <div class="left-footer">
+        <div class="top-left">
+          <div class="logo-footer">
+            <a href=""><img src="img/logo-footer.png" alt="" style="width:92.92px; height:25.69px;"></a>
+          </div>
+          <a href=""><img src="img/dmca.png" alt=""style="width: 61.33px; height:19.77px;"></a>
+          <div class="logo-cong-thuong">
+            <a href=""><img src="img/img-congthuong.png" alt=""style="width:70.52px; height:26.36px;"></a>
+          </div>
+        </div>
+        <div class="content-left-ft content-ft">
+          <div class="info-left-ft">
+            <p>Công ty Cổ phần Dự Kim với số đăng ký kinh doanh: 0105777650</p>
+            <p><strong>Địa chỉ đăng ký:</strong>Tổ dân phố Tháp, P.Đại Mỗ, Q.Nam Từ Liêm, TP.Hà Nội, Việt Nam</p>
+            <p><strong>Số điện thoại:</strong>0243 205 2222/ 090 589 8683</p>
+            <p><strong>Email:</strong>cskh@ivy.com.vn</p>
+          </div>
+          <ul class="list-social">
+            <li><a href=""><img src="img/fb.svg" alt="" style="width:12px; height:24px;"></a></li>
+            <li><a href=""><img src="img/ic_gg.svg" alt="" style="width:22px; height:22px;"></a></li>
+            <li><a href=""><img src="img/ic_instagram.svg" alt="" style="width:30px; height:30px;"></a></li>
+            <li><a href=""><img src="img/ic_pinterest.svg" alt="" style="width:27px; height:27px;"></a></li>
+            <li><a href=""><img src="img/ic_ytb.svg" alt="" style="width:24px; height:17px;"></a></li>
+          </ul>
+          <div class="hotline"></div>
+        </div>
+      </div>
+    </div>
+    <div class="center-footer" d-flex>
+      <div class="left-center-ft item-center-ft"></div>
+      <div class="main-center-ft item-center-ft"></div>
+      <div class="right-center-ft item-center-ft"></div>
+    </div>
+    <div class="right-footer"></div>
+  </footer>
+  </div>
+    <script src="footer.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ View/Pages/payment/pay.php
@@ -0,0 +1,24 @@
+<?php include '../../Component/header/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <link rel="stylesheet" href="pay.css">
+  <link rel="stylesheet" href="../../Component/header/header.css">
+  <script src="pay.js"></script>
+  <title>Ivy</title>
+</head>
+<body>
+    <section id="Slider">
+        <div class="slider-container">
+            
+        </div>
+    </section>
+    <script src="pay.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
