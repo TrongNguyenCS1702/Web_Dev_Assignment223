3613e1e9b583f259896728b2dfab5e55a4e9415e
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-08-09 22:56:16 +0700

    update home

--- View/Component/footer/footer.php
+++ View/Component/footer/footer.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <link rel="stylesheet" href="footer.css">
   <script src="footer.js"></script>
+  <link rel="stylesheet" href="footer.css">
   <title>Ivy</title>
 </head>
 <body>
@@ -18,11 +18,11 @@
       <div class="left-footer">
         <div class="top-left">
           <div class="logo-footer">
-            <a href=""><img src="img/logo-footer.png" alt="" style="width:92.92px; height:25.69px;"></a>
+            <a href=""><img src="/View/Component/footer/img/logo-footer.png" alt="" style="width:92.92px; height:25.69px;"></a>
           </div>
-          <a href=""><img src="img/dmca.png" alt=""style="width: 61.33px; height:19.77px;"></a>
+          <a href=""><img src="/View/Component/footer/img/dmca.png" alt=""style="width: 61.33px; height:19.77px;"></a>
           <div class="logo-cong-thuong">
-            <a href=""><img src="img/img-congthuong.png" alt=""style="width:70.52px; height:26.36px;"></a>
+            <a href=""><img src="/View/Component/footer/img/img-congthuong.png" alt=""style="width:70.52px; height:26.36px;"></a>
           </div>
         </div>
         <div class="content-left-ft content-ft">
@@ -33,11 +33,11 @@
             <p><strong>Email:</strong>cskh@ivy.com.vn</p>
           </div>
           <ul class="list-social">
-            <li><a href=""><img src="Web_Dev_Assignment223/View/" alt="" style="width:12px; height:24px;"></a></li>
-            <li><a href=""><img src="img/ic_gg.svg" alt="" style="width:22px; height:22px;"></a></li>
-            <li><a href=""><img src="img/ic_instagram.svg" alt="" style="width:30px; height:30px;"></a></li>
-            <li><a href=""><img src="img/ic_pinterest.svg" alt="" style="width:27px; height:27px;"></a></li>
-            <li><a href=""><img src="img/ic_ytb.svg" alt="" style="width:24px; height:17px;"></a></li>
+            <li><a href=""><img src="/View/Component/footer/img/fb.svg" alt="" style="width:12px; height:24px;"></a></li>
+            <li><a href=""><img src="/View/Component/footer/img/ic_gg.svg" alt="" style="width:22px; height:22px;"></a></li>
+            <li><a href=""><img src="/View/Component/footer/img/ic_instagram.svg" alt="" style="width:30px; height:30px;"></a></li>
+            <li><a href=""><img src="/View/Component/footer/img/ic_pinterest.svg" alt="" style="width:27px; height:27px;"></a></li>
+            <li><a href=""><img src="/View/Component/footer/img/ic_ytb.svg" alt="" style="width:24px; height:17px;"></a></li>
           </ul>
           <div class="hotline"></div>
         </div>
--- View/Component/header/header.php
+++ View/Component/header/header.php
@@ -7,12 +7,100 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
   <link rel="stylesheet" href="header.css">
-  <script src="index.js"></script>
   <title>Ivy</title>
+  <style>
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+li {
+    list-style: none;
+}
+a {
+    text-decoration: none;
+    color: black;
+}
+header {
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 50px;
+    height: 70px;
+    width: 100%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    transition: background-color 0.3s;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #FFF;
+    z-index: 20;
+}
+.menu{
+    flex: 3;
+    display: flex;
+}
+.menu li a {
+    padding: 0 12px;
+    position: relative;
+    display: block;
+    line-height: 20px;
+    font-size: 12px;
+}
+.sub-menu {
+    position: absolute;
+    width: 700px;
+    padding: 10px 0 10px 20px;
+    border: 1px solid #000;
+    visibility: hidden;
+    display: flex;
+    top: 50px;
+    transition: 0.3s;
+}
+.menu li:hover .sub-menu {
+    visibility: visible;
+    top: 70px;
+}
+.sub-menu ul a {
+    font-weight: normal;
+}
+.menu a{
+    font-size: 12px;
+    font-weight: bold;
+}
+.logo {
+    flex:1;
+}
+.orthers {
+    flex: 1;
+    display: flex;
+}
+.orthers a {
+    padding: 0 12px;
+}
+.form_search {
+    position: relative;
+}  
+.submit {
+    position: absolute;
+    right: 5px; /* Điều chỉnh vị trí của nút tìm kiếm so với ô nhập liệu */
+    top: 50%; /* Đưa nút tìm kiếm lên giữa theo chiều dọc */
+    transform: translateY(-50%); /* Đưa nút tìm kiếm lên giữa theo chiều dọc */
+    border: none;
+    background-color: #f8f9fa;
+}
+.orthers input {
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #000;
+}
+  </style>
 </head>
 <body>
   <div class="container">
-
     <header>  
       <div class="logo">
         <img src="/View/Component/images/logo.png" alt="logo">
@@ -93,7 +181,6 @@
       </div>
     </header>
   </div>
-    <script src="header.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
