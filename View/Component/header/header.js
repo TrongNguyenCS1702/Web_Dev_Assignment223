68bbe75f029b0afc32432778128387fd673ede3a
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-08-14 23:07:17 +0700

    update

--- View/Component/header/header.js
+++ View/Component/header/header.js
@@ -17,19 +17,3 @@ const maxOpacity = 0.4;
 const opacity = Math.min(1, 1 - (scrollPosition / 300) * maxOpacity);
 header.style.opacity = opacity.toFixed(2);
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Lắng nghe sự kiện DOMContentLoaded để đảm bảo mã JavaScript chỉ thực thi sau khi tất cả nội dung đã được tải.
-
-  const menuItems = document.querySelectorAll(".menu > li"); // Lấy tất cả các phần tử li trong menu.
-
-  menuItems.forEach((item) => {
-    const subMenu = item.querySelector(".sub-menu"); // Tìm sub-menu liên quan đến mỗi phần tử li.
-
-    item.addEventListener("click", () => {
-      if (subMenu) {
-        subMenu.classList.toggle("active"); // Toggle lớp 'active' để hiển thị hoặc ẩn sub-menu.
-      }
-    });
-  });
-});
