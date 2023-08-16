5e967a4377a393bfef3e9bc711bf165fe14f74e7
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-08-16 23:29:56 +0700

    update

--- View/Component/header/header.js
+++ View/Component/header/header.js
@@ -17,3 +17,59 @@ const maxOpacity = 0.4;
 const opacity = Math.min(1, 1 - (scrollPosition / 300) * maxOpacity);
 header.style.opacity = opacity.toFixed(2);
 });
+//
+// Lấy tham chiếu đến phần tử mobile-menu và sub-mobile-menu
+const mobileMenu = document.querySelector('.mobile-menu');
+const subMobileMenu = document.querySelector('.sub-mobile-menu');
+
+// Thêm sự kiện click cho phần tử mobile-menu
+mobileMenu.addEventListener('click', () => {
+    // Khi nhấp vào, chuyển đổi lớp active của mobile-menu
+    mobileMenu.classList.toggle('active');
+    
+    // Khi lớp active tồn tại, thêm lớp active cho sub-mobile-menu
+    if (mobileMenu.classList.contains('active')) {
+        subMobileMenu.classList.add('active');
+    } else {
+        subMobileMenu.classList.remove('active');
+    }
+});
+//
+// Lấy danh sách các phần tử có lớp "has-child"
+const hasChildItems = document.querySelectorAll('.has-child');
+
+// Đặt sự kiện click cho mỗi phần tử có lớp "has-child"
+hasChildItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Lấy phần tử con của "has-child" có lớp "sub-menu-mb"
+    const subMenu = item.querySelector('.sub-menu-mb');
+    
+    // Đảo ngược trạng thái hiển thị của menu con
+    subMenu.classList.toggle('active');
+    
+    // Lấy phần tử con có lớp "arrow"
+    const arrow = item.querySelector('.arrow');
+    
+    // Đảo ngược trạng thái hiển thị của dấu cộng và dấu trừ
+    arrow.classList.toggle('active');
+  });
+});
+// Lấy danh sách các phần tử có lớp "has-sub-child"
+const hasSubChildItems = document.querySelectorAll('.has-sub-child');
+
+// Đặt sự kiện click cho mỗi phần tử có lớp "has-sub-child"
+hasSubChildItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Lấy phần tử con của "has-sub-child" có lớp "child-sub"
+    const childSubMenu = item.querySelector('.child-sub');
+    
+    // Đảo ngược trạng thái hiển thị của menu con
+    childSubMenu.classList.toggle('active');
+    
+    // Lấy phần tử con có lớp "arrows"
+    const arrows = item.querySelector('.arrows');
+    
+    // Đảo ngược trạng thái hiển thị của dấu cộng và dấu trừ
+    arrows.classList.toggle('active');
+  });
+});
--- View/Component/header/header.php
+++ View/Component/header/header.php
@@ -8,180 +8,239 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
   <link rel="stylesheet" href="/View/Component/header/header.css">
   <title>Ivy</title>
-  <style>
-    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Arial, Helvetica, sans-serif;
-}
-li {
-    list-style: none;
-}
-a {
-    text-decoration: none;
-    color: black;
-}
-header {
-    
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 12px 50px;
-    height: 70px;
-    width: 100%;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.3s;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background: #FFF;
-    z-index: 20;
-}
-.menu{
-    flex: 3;
-    display: flex;
-}
-.menu li a {
-    padding: 0 12px;
-    position: relative;
-    display: block;
-    line-height: 20px;
-    font-size: 12px;
-}
-.sub-menu {
-    position: absolute;
-    width: 700px;
-    padding: 10px 0 10px 20px;
-    border: 1px solid #5a5a5a;
-    visibility: hidden;
-    display: flex;
-    top: 50px;
-    transition: 0.3s;
-    background-color: #f8f9fa;
-}
-.menu li:hover .sub-menu {
-    visibility: visible;
-    top: 70px;
-}
-.sub-menu ul a {
-    font-weight: normal;
-}
-.menu a{
-    font-size: 12px;
-    font-weight: bold;
-}
-.logo {
-    flex:1;
-}
-.orthers {
-    flex: 1;
-    display: flex;
-}
-.orthers a {
-    padding: 0 12px;
-}
-.form_search {
-    position: relative;
-}  
-.submit {
-    position: absolute;
-    right: 5px; /* Điều chỉnh vị trí của nút tìm kiếm so với ô nhập liệu */
-    top: 50%; /* Đưa nút tìm kiếm lên giữa theo chiều dọc */
-    transform: translateY(-50%); /* Đưa nút tìm kiếm lên giữa theo chiều dọc */
-    border: none;
-    background-color: #f8f9fa;
-}
-.orthers input {
-    width: 100%;
-    border: none;
-    border-bottom: 1px solid #000;
-}
-  </style>
 </head>
 <body>
-  <div class="container">
-    <header id="header">  
-      <div class="logo">
-        <a href="/View/Pages/Home/home.php"><img src="/View/Component/images/logo.png" alt="logo"></a>
+<div class="container">
+  <header id="header" class="site-header" role="banner" itemscope="" itemtype="http://schema.org/WPHeader">
+    <div class="container d-flex">
+        <nav class="main-menu" role="navigation" itemscope="" itemtype="http://schema.org/SiteNavigationElement">
+            <div class="mobile-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="menu">
+              <li>
+                <a href="javascript:void(0)">Nữ</a>
+                  <ul class="sub-menu">
+                    <div class="list-submenu d-flex">                                                                                                                                                                                                                                                                                              <div class="item-list-submenu">
+                      <h3><a href="https://ivymoda.com/danh-muc/ao-nu">ÁO</a></h3>
+                      <ul>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/ao-so-mi-nu">Áo sơ mi</a>
+                          </li>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/ao-thun-nu">Áo thun</a>
+                          </li>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/ao-kieu-nu">Áo kiểu</a>
+                          </li>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/ao-peplum-nu">Áo peplum</a>
+                          </li>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/ao-vest-nu">Áo vest/ blazer</a>
+                          </li>
+                      </ul>
+                    </div>
+                    <div class="item-list-submenu">
+                      <h3><a href="https://ivymoda.com/danh-muc/quan-nu">QUẦN &amp; JUMPSUIT</a></h3>
+                      <ul>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/quan-jean-nu">Quần jeans</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/quan-dai-nu">Quần dài</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/quan-lung-short-nu">Quần lửng/ short</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/quan-baggy-nu">Quần baggy</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/jumpsuit">Jumpsuit</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="item-list-submenu">
+                      <h3><a href="https://ivymoda.com/danh-muc/chan-vay-nu">CHÂN VÁY</a></h3>
+                      <ul>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/chan-vay">Chân váy</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/chan-vay-xep-ly">Chân váy xếp ly</a>
+                        </li>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/chan-vay-chu-A">Chân váy chữ A</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="item-list-submenu">
+                        <h3><a href="https://ivymoda.com/danh-muc/dam-nu">Giày/Dép</a></h3>
+                        <ul>
+                          <li>
+                            <a href="https://ivymoda.com/danh-muc/dam">Giày/Dép</a>
+                          </li>
+                        </ul>
+                    </div>
+                    <div class="item-list-submenu">
+                      <h3><a href="https://ivymoda.com/danh-muc/phu-kien">PHỤ KIỆN</a></h3>
+                      <ul>
+                        <li>
+                          <a href="https://ivymoda.com/danh-muc/tui-nu">PHỤ KIỆN</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </ul>
+              </li>
+              <li>
+                <a href="https://metagent.vn/" target="_blank">Nam</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)">Trẻ em</a>                        
+              </li>
+              <li class="d-none">
+                <a href="javascript:void(0)">Bộ sưu tập</a>
+              </li>
+              <li>
+                <a href="https://ivymoda.com/tin-tuc/tin-chinh">LIFESTYLE</a>
+              </li>
+              <li class="menu-custom">
+                <a href="javascript:void(0)">Về Chúng Tôi</a>
+              </li>
+            </ul>
+            <div class="sub-mobile-menu">
+              <div class="box-action">
+                <a href="https://ivymoda.com/customer/login" class="action-login">Đăng nhập</a>
+              </div>
+              <ul class="menu-mb">
+                  <li class="has-child">
+                    <a href="javascript:void(0)">Nữ<span class="arrow"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                      <ul class="sub-menu-mb">
+                        <li class="has-sub-child">
+                          <a href="https://ivymoda.com/danh-muc/ao-nu" target="_blank">ÁO<span class="arrows"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                          <ul class="child-sub">
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-so-mi-nu">Áo sơ mi</a>
+                              </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-thun-nu">Áo thun</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-kieu-nu">Áo kiểu</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-croptop">Áo croptop</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-peplum-nu">Áo peplum</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/ao-vest-nu">Áo vest/ blazer</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="has-sub-child">
+                          <a href="https://ivymoda.com/danh-muc/quan-nu">QUẦN &amp; JUMPSUIT<span class="arrows"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                          <ul class="child-sub">
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/quan-jean-nu">Quần jeans</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/quan-dai-nu">Quần dài</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/quan-lung-short-nu">Quần lửng/ short</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/quan-baggy-nu">Quần baggy</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/jumpsuit">Jumpsuit</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="has-sub-child">
+                          <a href="https://ivymoda.com/danh-muc/chan-vay" target="_blank">CHÂN VÁY<span class="arrows"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                          <ul class="child-sub">
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/chan-vay">Chân váy</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/chan-vay-xep-ly">Chân váy xếp ly</a>
+                            </li>
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/chan-vay-chu-A">Chân váy chữ A</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="has-sub-child">
+                          <a href="https://ivymoda.com/danh-muc/dam-nu" target="_blank">Giày/Dép<span class="arrows"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                          <ul class="child-sub">
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/dam">Giày/Dép</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="has-sub-child">
+                          <a href="https://ivymoda.com/danh-muc/phu-kien">PHỤ KIỆN<span class="arrows"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                          <ul class="child-sub">
+                            <li>
+                              <a href="https://ivymoda.com/danh-muc/phu-kien-nu">Phụ kiện</a>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                  </li>
+                  <li class="has-child">
+                    <a href="https://metagent.vn/" target="_blank">Nam<span class="arrow"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>                        
+                  </li>
+                  <li class="has-child">
+                    <a href="javascript:void(0)">Trẻ em<span class="arrow"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                  </li>
+                  <li li class="has-child">
+                    <a href="javascript:void(0)">Bộ sưu tập</a>
+                  </li>
+                  <li>
+                    <a href="https://ivymoda.com/tin-tuc/tin-chinh">LIFESTYLE</a>
+                  </li>
+                  <li class="has-child">
+                    <a href="javascript:void(0)">Về Chúng Tôi<span class="arrow"><i class="fas fa-plus"></i><i class="fas fa-minus"></i></span></a>
+                  </li>
+                </ul>
+              <div class="close-menu"><i class="fas fa-times"></i></div>
+            </div>
+          </nav>
+        <!-- .main-menu -->
+        <div class="site-brand">
+            <a href="https://ivymoda.com/"><img src="https://pubcdn.ivymoda.com/ivy2/images/logo.png" alt="Tin tức | IVY moda"></a>
+        </div>
+        <div class="right-header">
+          <form class="search-form" enctype="application/x-www-form-urlencoded" method="get" action="" name="frm_search">
+              <button class="submit"><i class="fas fa-search"></i></button>
+              <input id="search-quick" type="text" name="q" placeholder="TÌM KIẾM SẢN PHẨM" autocomplete="off" minlength="1" style="padding-left: 40px;">
+          </form>
+          <div class="header-actions">
+            <!-- .item wallet -->
+            <div class="item wallet">
+              <a class="icon" href="#"><i class="fas fa-user"></i></a>
+            </div>
+            <!-- .item wallet -->
+            <div class="item item-cart">
+              <a class="icon" href="#"><i class="fas fa-shopping-bag"></i>
+                <span class="number-cart">0</span>
+              </a>
+            </div>
+          </div>
       </div>
-      <div class="menu">
-        <li><a href="#">NỮ</a>
-          <ul class="sub-menu">
-            <li><a href="#">Áo</a>
-              <ul>
-                <li><a href="/View/Pages/delivery/delivery.php">Áo sơ mi</li>
-                <li><a href="">Áo thun</a></li>
-                <li><a href="">Áo Vest</a></li>
-                <li><a href="">Áo khóac</a></li>
-                <li><a href="">Áo Len</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Quần</a>
-              <ul>
-                <li><a href="">Áo sơ mi</a></li>
-                <li><a href="">Áo thun</a></li>
-                <li><a href="">Áo Vest</a></li>
-                <li><a href="">Áo khóac</a></li>
-                <li><a href="">Áo Len</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Áo</a>
-              <ul>
-                <li><a href="">Áo sơ mi</a></li>
-                <li><a href="">Áo thun</a></li>
-                <li><a href="">Áo Vest</a></li>
-                <li><a href="">Áo khóac</a></li>
-                <li><a href="">Áo Len</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Áo</a>
-              <ul>
-                <li><a href="">Áo sơ mi</a></li>
-                <li><a href="">Áo thun</a></li>
-                <li><a href="">Áo Vest</a></li>
-                <li><a href="">Áo khóac</a></li>
-                <li><a href="">Áo Len</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Áo</a>
-              <ul>
-                <li><a href="">Áo sơ mi</a></li>
-                <li><a href="">Áo thun</a></li>
-                <li><a href="">Áo Vest</a></li>
-                <li><a href="">Áo khóac</a></li>
-                <li><a href="">Áo Len</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="#">NAM</a></li>
-        <li><a href="#">TRẺ EM</a></li>
-        <li><a href="#">SALE CUỐI TUẦN</a></li>
-        <li> <a href="#">BỘ SƯU TẬP</a></li>
-        <li><a href="#">LIFESTYLE</a></li>
-        <li><a href="#">VỀ CHÚNG TÔI</a></li>
-      </div>
-      <div class="orthers">
-          <span class="search">
-              <form action="#" method="get" enctype="#" name="form_search" class="form_search">
-                  <input class="input" type="text" name="search" placeholder="Tìm kiếm " autocomplete="off" minlength="1">
-                  <button class="submit"><i class="fas fa-search"></i></button>
-              </form>
-          </span>
-          <span class="item wallet">
-              <a class="icon" href="#"><i class="fas fa-headphones-alt"></i></a>
-          </span>
-          <span class="item wallet">
-              <a class="icon" href="/View/Pages/Login/login.php"><i class="fas fa-user"></i></a>
-          </span>
-          <span class="item wallet">
-              <a class="icon" href="/View/Pages/cart/cart.php"><i class="fas fa-shopping-bag"></i></a>
-          </span>
-      </div>
-    </header>
-  </div>
+        <!-- .right-header -->
+    </div>
+    <!-- .container -->
+  </header>
+</div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
     <script src="/View/Component/header/header.js"></script>
 </body>
